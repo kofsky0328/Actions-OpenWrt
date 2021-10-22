e66364a48632c0c6124d19c1cd94fff428009435
kofsky0328 <32171012+kofsky0328@users.noreply.github.com>
2021-10-22 23:52:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/coolsnowwolf/lede
-cp -r /home/root/lede/package/lean /home/root/openwrt/package/lean
+./scripts/feeds update -a
+./scripts/feeds install -a
+cp -r ./lede/package/lean ./openwrt/package/lean
