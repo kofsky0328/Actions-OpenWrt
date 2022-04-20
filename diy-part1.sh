2017ec67a843b2385fb7df5d85921e41f5ddd4ac
kofsky0328 <32171012+kofsky0328@users.noreply.github.com>
2022-04-21 00:28:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-  svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/ package/lean
-# make dirclean
-  rm -rf package/lean/mt
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
