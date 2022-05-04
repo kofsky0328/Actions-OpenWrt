e13d92911473c556331f7ad9c7a5e69ccf87469a
kofsky0328 <32171012+kofsky0328@users.noreply.github.com>
2022-05-05 02:13:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,5 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+git clone https://github.com/cokebar/openwrt-vlmcsd.git package/openwrt-vlmcsd
+git clone https://github.com/cokebar/luci-app-vlmcsd.git package/luci-app-vlmcsd
