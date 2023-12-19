af0064e38220c742a442f0f042734161a7ef3e7c
kofsky0328 <32171012+kofsky0328@users.noreply.github.com>
2023-12-19 23:59:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 git clone https://github.com/cokebar/openwrt-vlmcsd.git package/openwrt-vlmcsd
 git clone https://github.com/cokebar/luci-app-vlmcsd.git package/luci-app-vlmcsd
 sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/' target/linux/x86/Makefile
