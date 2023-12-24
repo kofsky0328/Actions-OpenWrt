35bf7cdd3d91182f75be880f5755fdceaf929adc
kofsky0328 <32171012+kofsky0328@users.noreply.github.com>
2023-12-24 15:59:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/softethervpn-server/softethervpn5-server/' feeds/smpackage/luci-app-softethervpn/Makefile
+# sed -i 's/softethervpn-server/softethervpn5-server/' feeds/smpackage/luci-app-softethervpn/Makefile
