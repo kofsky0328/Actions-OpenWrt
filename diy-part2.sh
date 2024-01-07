37831b7f65256807fbe556b638eefb60fa9b4493
kofsky0328 <32171012+kofsky0328@users.noreply.github.com>
2024-01-07 23:37:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,6 @@ sed -i 's/softethervpn-server/softethervpn5-server/' feeds/smpackage/luci-app-so
 sed -i 's/PKG_VERSION:=5.02.5180/PKG_VERSION:=5.02.5181/' feeds/packages/net/softethervpn5/Makefile
 sed -i 's/PKG_HASH:=b5649a8ea3cc6477325e09e2248ef708d434ee3b2251eb8764bcfc15fb1de456/PKG_HASH:=2222ef48b3f9102265ef7d27e496ad40a1bd1eaba8093bc5e696b48402c52441/'  feeds/packages/net/softethervpn5/Makefile
 rm feeds/packages/net/softethervpn5/patches/010-engines.patch
-# cd ..
-# mv 150_remove-restriction.patch openwrt/feeds/packages/net/softethervpn5/patches/150_remove-restriction.patch
-# cd openwrt
+cd ..
+mv 150_remove-restriction.patch openwrt/feeds/packages/net/softethervpn5/patches/150_remove-restriction.patch
+cd openwrt
