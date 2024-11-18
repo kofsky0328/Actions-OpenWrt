2515af4fa2c9029a9cded3b8130080abdece92b2
kofsky0328 <32171012+kofsky0328@users.noreply.github.com>
2024-11-19 05:29:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/softethervpn-server/softethervpn5-server/' feeds/smpackage/luci-app-softethervpn/Makefile
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 # sed -i 's/PKG_VERSION:=5.02.5180/PKG_VERSION:=5.02.5181/' feeds/packages/net/softethervpn5/Makefile
 # sed -i 's/PKG_HASH:=b5649a8ea3cc6477325e09e2248ef708d434ee3b2251eb8764bcfc15fb1de456/PKG_HASH:=2222ef48b3f9102265ef7d27e496ad40a1bd1eaba8093bc5e696b48402c52441/'  feeds/packages/net/softethervpn5/Makefile
 # rm feeds/packages/net/softethervpn5/patches/010-engines.patch
